20d3ce08964dae29dd3a117afeb8dd769633dcb8
FastmanTheDuke <support@mdxp.io>
2026-03-14 14:17:43 +0100

    settings css

--- version-react/frontend/tailwind.config.js
+++ version-react/frontend/tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'codeclic-blue': '#00818a', // Couleur du flyer [cite: 30, 40]
+      },
+    },
   },
   plugins: [],
 }
-
