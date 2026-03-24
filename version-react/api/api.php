6fb4eb02952093584c4abaaa4e3798df3aa36c30
FastmanTheDuke <support@mdxp.io>
2026-03-24 10:31:10 +0100

    remove to git

--- version-react/api/api.php
+++ version-react/api/api.php
@@ -33,41 +33,7 @@ try {
             $mail = new PHPMailer(true);
             try {
                 // Paramètres du serveur (Exemple Mailtrap ou Gmail)
-                $mail->isSMTP();
-                $mail->Host = 'smtp.gmail.com'; // Remplacez par votre hôte
-                $mail->SMTPAuth = true;
-                $mail->Username = 'support@mdxp.io';      // Remplacez par votre user
-                $mail->Password = 'bszophzxgwliqian';  // Remplacez par votre pass
-                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-                $mail->Port = 587;
 
-                // Contournement du problème de certificat SSL en local (Windows/XAMPP)
-                $mail->SMTPOptions = array(
-                    'ssl' => array(
-                        'verify_peer' => false,
-                        'verify_peer_name' => false,
-                        'allow_self_signed' => true
-                    )
-                );
-
-                // Destinataires
-                $mail->setFrom('no-reply@mdxp.io', 'Code-Clic Plateforme');
-                $mail->addAddress('support@mdxp.io'); // Email de réception
-
-                // Contenu
-                $mail->isHTML(true);
-                $mail->Subject = "Nouvelle demande d'inscription - Code-Clic";
-                
-                // Sécurisation et conversion des sauts de ligne en balises <br>
-                $messageHtml = nl2br(htmlspecialchars($data->message));
-                
-                $mail->Body = "<h3>Nouvelle inscription reçue</h3>
-                                  <p><b>Nom :</b> {$data->prenom} {$data->nom}</p>
-                                  <p><b>Email :</b> {$data->email}</p>
-                                  <p><b>Statut :</b> {$data->statut}</p>
-                                  <p><b>Message :</b><br>{$messageHtml}</p>";
-
-                $mail->send();
                 $emailStatus = "Email envoyé";
             } catch (Exception $e) {
                 $emailStatus = "Erreur email : {$mail->ErrorInfo}";
